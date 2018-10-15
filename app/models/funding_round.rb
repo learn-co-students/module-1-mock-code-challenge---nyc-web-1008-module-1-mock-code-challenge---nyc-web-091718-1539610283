c73ae0eb5aad7f0363126d8755332a2fbd5b0bb6
magic-onion <alexander.m.truong@gmail.com>
2018-10-15 10:36:36 -0400

    done

--- app/models/funding_round.rb
+++ app/models/funding_round.rb
@@ -1,3 +1,24 @@
 class FundingRound
 
+  @@all = []
+
+  attr_accessor :type, :amount, :startup, :venture_capitalist
+
+  def initialize(type, amount=0, startup, venture_capitalist)
+    @type = type
+    @amount = amount
+    @startup = startup
+    @venture_capitalist = venture_capitalist
+    @@all << self
+  end
+
+  def self.all
+    @@all
+  end
+
+  def investment
+    @amount
+  end
+
+
 end
--- app/models/startup.rb
+++ app/models/startup.rb
@@ -1,3 +1,64 @@
 class Startup
 
+  attr_accessor :domain, :name
+  attr_reader :founder
+
+  @@all = []
+
+  def initialize(name, founder, domain)
+    @name = name
+    @founder = founder
+    @domain = domain
+    @investors_list = []
+    @funds_received = 0.0
+    @@all << self
+  end
+
+  def self.all
+    @@all
+  end
+
+  #matches string argument to initialization argument
+  def self.find_by_founder(name)
+    @@all.find {|startups| startups.founder == name}
+  end
+
+  #iterates over all startups, selecting all the domains and returning only unique items
+  def self.domains
+    domains = []
+    @@all.each {|startup| domains << startup.domain}
+      domains
+  end
+
+  #sets a new domain and name for the instance variables related to Start class
+  def pivot(new_domain, new_name)
+    self.domain = new_domain
+    self.name = new_name
+  end
+
+  #self is the instance object of Startup class
+  #it still needs to be passed into the FundingRound init method
+  #adds the VC to the investors array
+  def sign_contract(type, amount, venture_capitalist)
+    new_round = FundingRound.new(type, amount.to_f, self, venture_capitalist)
+    @investors_list << venture_capitalist
+    @funds_received += amount
+    new_round
+  end
+
+  #doesn't work yet! need to refactor connection between vc funding and startup receiving (through FundingRound)
+  def total_funds
+    @funds_received
+  end
+
+  #gives a return value that is not pretty, good start though
+  def investors
+    @investors_list
+  end
+
+
+  def big_investors
+    @investors_list.select {|investors| VentureCapitalist.tres_commas.include?(investors)}
+  end
+
 end
