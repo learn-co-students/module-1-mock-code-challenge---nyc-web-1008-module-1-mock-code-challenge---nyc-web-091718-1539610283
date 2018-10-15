61c3a693afbb6204ee20d9adab2435a0e718a7eb
magic8baller <lturis@gmail.com>
2018-10-15 10:36:48 -0400

    mock code chall

--- app/models/funding_round.rb
+++ app/models/funding_round.rb
@@ -1,3 +1,35 @@
 class FundingRound
 
+  attr_accessor :investment #?can it change
+  attr_reader :startup, :venture_capitalist, :type
+
+  @@all = []
+
+  def initialize(startup)
+    @startup = startup
+    @type = type
+    @investment = investment
+    @venture_capitalist = venture_capitalist
+    @@all << self #funding rounds
+  end
+
+  def startup
+    @startup
+  end
+
+  def venture_capitalist(venture_capitalist)
+    @venture_capitalist
+  end
+
+  def type
+    @type
+  end
+
+  def investment
+    @investment = investment.to_f
+  end
+
+  def self.all
+    @@all
+  end
 end
--- app/models/venture_capitalist.rb
+++ app/models/venture_capitalist.rb
@@ -1,3 +1,34 @@
 class VentureCapitalist
+  attr_accessor :name
+
+  @@all = []
+  @@tres_commas_club = [] #need to find way to add VC members
+
+  def initialize(name)
+    @name = name
+    @@all << self
+    @@tres_commas_club #?
+  end
+
+  def name
+    @name
+  end
+
+  def tres_commas
+    #if tres commas club .include?(VC name) ? true : false
+    #@@tres_commas_club << those VC's -??
+  end
+
+  def self.all
+    @@all
+  end
+
+  def self.tres_commas_club
+    @@tres_commas_club
+  end
+
+  def offer_contract(startup, type, investment)
+    new_round = Startup.new()
+  end
 
 end
