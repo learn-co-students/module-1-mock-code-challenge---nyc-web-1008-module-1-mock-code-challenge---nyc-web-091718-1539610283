576cdb1daf5ce30b81cb7093c3b41b019fe2b36d
Rexman17 <a.stone1794@gmail.com>
2018-10-15 10:35:29 -0400

    Done.

--- app/models/funding_round.rb
+++ app/models/funding_round.rb
@@ -1,3 +1,33 @@
+# This is the JOIN TABLE
+# FundingRound belongs_to VCs and Startups so it should have an attr_accessor for both
 class FundingRound
+  # attr_accessor
+  attr_accessor :startup, :venture_capitalist
+  attr_reader :round, :type
 
-end
+  @@all = []
+
+  def initialize(startup, venture_capitalist, round, type)
+    @startup = startup
+    @venture_capitalist = venture_capitalist
+    @round = round
+    @type = type
+
+    @@all << self
+  end
+
+  # -- STILL MUST DO --
+  # - `FundingRound#investment`
+  #   - returns a **number** that is the amount invested during this funding round - is this number in the initialization???
+    # or somewhere in the VC class??????????
+  #   - This should be a float that is not a negative number.
+
+  def self.all
+    @@all
+  end
+
+
+
+
+
+end # end of FundingRound class
--- app/models/venture_capitalist.rb
+++ app/models/venture_capitalist.rb
@@ -1,3 +1,37 @@
 class VentureCapitalist
+  attr_reader :name
+  attr_accessor :tres_commas
 
-end
+  @@all = []
+
+  def initialize(name, tres_commas = false)
+    @name = name
+    @tres_commas = tres_commas # how the heck do we know this? I'm just making it default option to false and we can change it if true
+    @@all << self
+  end
+
+  def self.all
+    @@all
+  end
+
+  def self.tres_commas_club # SELECT the VCs in tres commas club
+    @@all.select do |vc_object|
+        vc_object.tres_commas == true
+    end
+  end
+
+end # end of VC class
+
+
+#### VentureCapitalist
+
+# - `VentureCapitalist#offer_contract`
+#   - given a **startup object**, type of investment (as a string), and the amount invested (as a float), creates a new funding round and associates it with that startup and venture capitalist.
+# - `VentureCapitalist#funding_rounds`
+#   - returns an array of all funding rounds for that venture capitalist
+# - `VentureCapitalist#portfolio`
+#   - Returns a **unique** list of all startups this venture capitalist has funded
+# - `VentureCapitalist#biggest_investment`
+#   - returns the largest funding round given by this venture capitalist
+# - `VentureCapitalist#invested`
+#   - given a **domain string**, returns the total amount invested in that domain
