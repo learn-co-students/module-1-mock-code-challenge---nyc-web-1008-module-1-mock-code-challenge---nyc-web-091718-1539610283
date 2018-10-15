05e11cfa8df8d19465b0dfabcf7b544543285c0b
no0ro <nrolwes@gmail.com>
2018-10-15 10:41:34 -0400

    My commit

--- app/models/venture_capitalist.rb
+++ app/models/venture_capitalist.rb
@@ -1,3 +1,22 @@
 class VentureCapitalist
+  attr_accessor :vc_name
 
+  def initialize
+  end
+
+  def name
+  end
+
+  def tres_commas
+    if 
+  end
 end
+
+- `VentureCapitalist#name`
+  - returns a **string** that is the venture capitalist's name
+- `VentureCapitalist#tres_commas`
+  - returns a **boolean** stating whether or not the venture capitalist is in the Trés Commas club
+- `VentureCapitalist.all`
+  - returns an array of all venture capitalists
+- `VentureCapitalist.tres_commas_club`
+  - returns an array of all venture capitalists in the Trés Commas club
