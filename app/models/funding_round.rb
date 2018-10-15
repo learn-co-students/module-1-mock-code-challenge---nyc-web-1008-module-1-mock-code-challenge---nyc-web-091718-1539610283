86ae5b64acaed32af7ad36271cf4c12e10c4873d
Veraprot <vera.protopopova@gmail.com>
2018-10-15 10:36:04 -0400

    practice test

--- app/models/funding_round.rb
+++ app/models/funding_round.rb
@@ -1,3 +1,21 @@
 class FundingRound
+  attr_accessor :type 
+  attr_reader :startup, :venture_capitalist
+  @@all = []
 
+  def initialize(startup, venture_capitalist, type, amount)
+    @startup = startup
+    @venture_capitalist = venture_capitalist
+    @type = type
+    @amount = amount
+    @@all << self 
+  end 
+
+  def self.all 
+    @@all
+  end 
+
+  def investment 
+    @amount
+  end 
 end
--- app/models/startup.rb
+++ app/models/startup.rb
@@ -1,3 +1,52 @@
+require 'pry'
 class Startup
+  attr_accessor :name, :domain
+  attr_reader :founder 
+  @@all = []
+
+  def initialize(name, founder, domain)
+    @name = name
+    @founder = founder  
+    @domain = domain
+    @@all << self 
+  end 
+
+  def pivot(name, domain) 
+    self.name = name 
+    self.domain = domain
+  end 
+
+  def self.all
+    puts @@all
+  end
+  
+  #its not working i have no idea why 
+  # def self.find_by_founder(founder_name)
+  #   self.all.select do |startup|
+  #     startup.founder = founder_name 
+  #   end 
+  # end 
+
+  # def self.domains 
+  #   self.all.map do |startup|
+  #    startup.domain
+  #   end 
+  # end 
+  
+  def sign_contract(venture_capitalist, investment_type, amount)
+    funding = FundingRound.new(self, venture_capitalist, investment_type, amount)
+  end 
+
+  def num_funding_rounds
+    FundingRound.all.select do |round|
+      round.startup = self
+    end 
+  end 
+
 
 end
+
+# Startup.find_by_founder('vera')
+
+# Startup.domains
+ 
