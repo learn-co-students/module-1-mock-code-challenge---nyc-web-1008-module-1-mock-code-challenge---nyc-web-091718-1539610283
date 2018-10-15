ffc3330d7534dbd896c992c124e33f3a9a74a309
yoshinator <yoanante@gmail.com>
2018-10-15 10:36:10 -0400

    tools/console.rb:19:in FundingRound' for main:Object (NoMethodError)

--- app/models/funding_round.rb
+++ app/models/funding_round.rb
@@ -1,3 +1,30 @@
 class FundingRound
+  # FundingRound#venture_capitalist
+    # returns the venture capitalist object for that given funding round
+    # Once a funding round is created, I should not be able to change the venture capitalist
+  # FundingRound#startup
+    # returns the startup object for that given funding round
+    # Once a funding round is created, I should not be able to change the startup
+  # FundingRound#type
+    # returns a string that is the type of funding round
+    # Examples include: Angel, Pre-Seed, Seed, Series A, Series B, Series C, etc.
+  # FundingRound#investment
+    # returns a number that is the amount invested during this funding round
+    # This should be a float that is not a negative number.
+  attr_reader :startup, :venture_capitalist, :type, :investment
+  @@all = []
+  def initialize(startup, venture_capitalist,type)
+    @startup = startup #object
+    @venture_capitalist = venture_capitalist #object
+    @type = type #string
+    @investment  = investment #Positive float
+    @@all << self
+  end
+
+  # FundingRound.all
+  # returns all of the funding rounds
+  def self.all
+    @@all
+  end
 
 end
--- app/models/startup.rb
+++ app/models/startup.rb
@@ -1,3 +1,78 @@
 class Startup
+  # Startup#name
+  # returns a string that is the startup's name
+  # Startup#founder
+  # returns a string that is the founder's name
+  # Once a startup is created, the founder cannot be changed.
+  # Startup#domain
+  # returns a string that is the startup's domain
+  # Once a startup is created, the domain cannot be changed.
+
+  attr_accessor :name
+  attr_reader :founder, :domain
+
+  @@all = []
+
+  def initialize(name, founder, domain)
+    @name = name
+    @founder = founder
+    @domain = domain
+    @@all << self
+  end
+
+  #Sign_contract
+  # given a venture capitalist object, type of investment (as a string), and the amount invested (as a float), creates a new funding round and associates it with that startup and venture capitalist.
+  def sign_contract(venture_capitalist, investment_type, investment_amount)
+    FundingRound.new(self, venture_capitalist, investment_type, investment_amount)
+  end
+
+  def funding_rounds
+    FundingRound.all.select {|round| round.startup == self}
+  end
+  #num_funding_rounds
+  # Returns the total number of funding rounds that the startup has gotten
+  def num_funding_rounds
+    funding_rounds.count
+  end
+
+  #total_funds
+  # Returns the total sum of investments that the startup has gotten
+  def total_funds
+    total = 0.0
+    funding_rounds.each {|round| total += round.investment}
+  end
+  #investors
+  # Returns a unique array of all the venture capitalists that have invested in this company
+  def investors
+    funding_rounds.uniq {|round| round.venture_capitalist}
+  end 
+  # Startup#big_investors
+  # Returns a unique array of all the venture capitalists that have invested in this company and are in the Trés Commas club
+
+  # Startup#pivot
+  # given a string of a domain and a string of a name, change the domain and name of the startup
+  def pivot(domain, name)
+    self.domain = domain
+    self.name = name
+
+  end
+  # Startup.all
+  # should return all of the startup instances
+  def self.all
+    @@all
+  end
+
+  # Startup.find_by_founder
+  # given a string of a founder's name, returns the first startup whose founder's name matches
+  def self.find_by_founder(founder)
+    @@all.find {|startup| startup.founder == founder}
+  end
+
+  # Startup.domains
+  # should return an array of all of the different startup domains
+
+  def self.domains
+    @@all.map {|startup| startup.domain}\
+  end
 
 end
